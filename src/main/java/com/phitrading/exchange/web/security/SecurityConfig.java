0041958681d7eed9f11eb0e19ec27ba2d5576039
velizarmitov <v.mitov9898@gmail.com>
2025-12-04 21:57:53 +0200

    refactor SecurityConfig adding ADMIN

--- src/main/java/com/phitrading/exchange/web/security/SecurityConfig.java
+++ src/main/java/com/phitrading/exchange/web/security/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
             .formLogin(form -> form
                 .loginPage("/auth/login")
                 .loginProcessingUrl("/auth/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
                 .defaultSuccessUrl("/dashboard", true)
                 .permitAll()
             )
